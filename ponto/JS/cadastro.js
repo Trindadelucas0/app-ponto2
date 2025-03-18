b1f5dfd44621ee2bd3098f8890c1912cd7a9a39c
Lucas Rodrigues <Lucasrodrigues4@live.com>
2025-03-18 10:44:29 -0300

    corrigindo formatação

--- ponto/JS/cadastro.js
+++ ponto/JS/cadastro.js
@@ -6,19 +6,23 @@ document.addEventListener('DOMContentLoaded', function() {
         const email = document.getElementById('email').value;
         const senha = document.getElementById('senha').value;
         const erro = document.getElementById("erro");
+        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
         if (!nome || !email || !senha) {
             alert('Por favor, preencha todos os campos!');
             return;
         }
-        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        
         if (!regexEmail.test(email)) {
             erro.textContent = "E-mail inválido! Digite um e-mail correto.";
             return;
         }
-        erro.textContent = "";
+            erro.textContent = "";
+
+
         let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
+        
         const emailExistente = usuarios.some(usuario => usuario.email === email);
         if (emailExistente) {
             alert('Este e-mail já está cadastrado!');
@@ -27,9 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const novoUsuario = { nome, email, senha };
         usuarios.push(novoUsuario);
-
         localStorage.setItem('usuarios', JSON.stringify(usuarios));
-
         alert('Usuário cadastrado com sucesso!');
         window.location.href = 'login.html';
     });
