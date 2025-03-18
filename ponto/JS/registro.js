83ffbb54208dc0a3612ba48ba5861708233bc358
Lucas Rodrigues <Lucasrodrigues4@live.com>
2025-03-18 11:16:09 -0300

    FEAT: ADICIONADO A CONFIRMAÇÃO DE REGISTRO

--- ponto/JS/registro.js
+++ ponto/JS/registro.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
             alert('Nenhum usuário logado! Faça login primeiro.');
             return;
         }
-
+        const nome = document.getElementById('nome').value.trim();
         const data = document.getElementById('data').value.trim();
         const tipoPonto = document.getElementById('tipo-ponto').value.trim();
         const hora = document.getElementById('hora').value.trim();
@@ -18,28 +18,39 @@ document.addEventListener('DOMContentLoaded', function() {
             return;
         }
 
+        const confirmar=confirm(`${nome} Deseja prosseguir com os dados? 
+            Data: ${data}
+            Tipo de Ponto: ${tipoPonto}
+            Horário: ${hora} `)
+            if(!confirmar){
+                alert(`REGISTRO CANCELADO!
+                    TENTE NOVAMENTE!`)
+               
+            }
+
         let registros = JSON.parse(localStorage.getItem('registros')) || {};
         if (!registros[usuarioLogado]) {
             registros[usuarioLogado] = [];
         }
-
         const fotoBase64 = [];
         Array.from(foto).forEach(file => {
             const reader = new FileReader();
             reader.onload = function(event) {
                 fotoBase64.push(event.target.result);
-
+                
                 if (fotoBase64.length === foto.length) {
                     const novoRegistro = { data, tipoPonto, hora, foto: fotoBase64 };
                     registros[usuarioLogado].push(novoRegistro);
                     localStorage.setItem('registros', JSON.stringify(registros));
-
+                    
                     alert('Ponto registrado com sucesso!');
                     window.location.href = 'ver-registros.html';  // Redireciona para a página de registros
                 }
             };
             reader.readAsDataURL(file);  // Lê a foto como base64
         });
+        
+        
     });
 
     // Navegar para a página de registros
